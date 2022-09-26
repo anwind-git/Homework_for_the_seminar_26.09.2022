9b0451485d1df871a0558c06715b77f39bde4076
anwind-git <work-git@yandex.ru>
2022-09-26 23:33:18 +0300

    принимает на вход число и выдаёт сумму цифр в числе

---
+++ task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+void Metod1()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int remains = 0;
+
+    for (int i = 1; number > 0; i++)
+    {
+        remains = remains + number % 10;
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе: " + remains);
+}
+Metod1();
