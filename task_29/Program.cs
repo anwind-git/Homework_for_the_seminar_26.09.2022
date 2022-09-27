3efcc48c116ce3e9ddd44e5ac2652ba3aa27d6e8
anwind-git <work-git@yandex.ru>
2022-09-27 14:51:37 +0300

    задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив

---
+++ task_29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив
+int[] array = new int[8];
+void RandomMas()
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(-99, 99);
+    string arr1 = string.Join(", ", array);
+    Console.WriteLine($"Массив до сортировки:    [" + arr1 + "]");
+}
+
+void SortMas()
+{
+    int temp;
+    for (int i = 0; i < array.Length; i++)
+        for (int j = i + 1; j < array.Length; j++)
+            if (Math.Abs(array[i]) > Math.Abs(array[j]))
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }                              
+    string arr2 = string.Join(", ", array);
+    Console.WriteLine($"Массив после сортировки: [" + arr2 + "]");
+}
+RandomMas();
+SortMas();
